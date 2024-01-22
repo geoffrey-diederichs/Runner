068aa94c0bde42d8ef50baf3bd87645fb987a2ea
geoffrey-diederichs <geoffrey.diederichs@ynov.com>
2024-01-22 10:21:08 +0100

    randomized obstacle + increase speed + increse spawn rate + limit destroying obstacles

--- Assets/Scripts/Obstacles.cs
+++ Assets/Scripts/Obstacles.cs
@@ -3,21 +3,20 @@ using UnityEngine;
 public class Obstacles : MonoBehaviour
 {
     private new Rigidbody2D rb;
-    private float speed = 0.25f;
-    private int lifetime = 5;
+    private float speed = 0.2f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
 
-        int rand = Random.Range(0,5);
+        int rand = Random.Range(0,4);
         if (rand == 0)
         {
-            transform.localScale = new Vector3(0.5f, 3.0f, 1.0f);
+            transform.localScale = new Vector3(1.0f, Random.Range(3,4), 1.0f);
         }
         else if (rand == 1)
         {
-            transform.localScale = new Vector3(5.0f, 1f, 1.0f);
+            transform.localScale = new Vector3(Random.Range(3,5), 1.0f, 1.0f);
         }
     }
 
@@ -28,6 +27,10 @@ public class Obstacles : MonoBehaviour
 
     private void Start()
     {
-        Destroy(this.gameObject, this.lifetime);
+    }
+
+    public void SetSpeed(float speedInc)
+    {
+        this.speed += speedInc;
     }
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class ObstaclesSpawner : MonoBehaviour
+{
+    public Obstacles obstacleModel;
+    private float spawnRate = 2.0f;
+    private float nextSpawn = 0.0f;
+    private float speedInc = 0.0f;
+
+    private void Start()
+    {
+        // InvokeRepeating(nameof(Spawn), this.spawnRate, this.spawnRate);
+    }
+
+    private void Update()
+    {
+        if (Time.time >= this.nextSpawn)
+        {
+            this.Spawn();
+            nextSpawn = Time.time + spawnRate;
+
+            if (spawnRate > 1.5f)
+            {
+                spawnRate -= 0.05f;
+            }
+        }
+    }
+
+    private void Spawn()
+    {
+        Obstacles obstacle = Instantiate(this.obstacleModel, this.transform.position, new Quaternion());
+        obstacle.SetSpeed(this.speedInc);
+        this.speedInc += 0.01f;
+    }
+}
