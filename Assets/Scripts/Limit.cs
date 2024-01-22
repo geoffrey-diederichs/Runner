0a1a9d6d88f9787add4ee5495e0f0b3af9de47d0
geoffrey-diederichs <geoffrey.diederichs@ynov.com>
2024-01-22 10:59:30 +0100

    added scoring systtem

--- Assets/Scripts/Limit.cs
+++ Assets/Scripts/Limit.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class Limit : MonoBehaviour
 {
+    public Score score;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "obstacle")
         {
             Destroy(collision.gameObject);
+            score.UpdateScore(100);
         }
     }
 }
